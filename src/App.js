7cb82fe99ed9d461a7b2efde06eaaa64dd7432aa
gajen <meetgajen@yahoo.com>
2021-08-15 16:26:35 -0400

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import NavBar from './components/NavBar';
 
 
 function App() {
   return (
-   <h2>Hi this is project</h2>
+  <NavBar />
   );
 }
 
